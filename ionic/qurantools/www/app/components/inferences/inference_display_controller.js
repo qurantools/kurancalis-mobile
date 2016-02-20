3948ce202f9c2d36cb76b59f2371f8e73f38daea
kurtuluso <kurtulus@posta.la>
2016-02-20 21:22:12 +0200

    async with web

--- ionic/qurantools/www/app/components/inferences/inference_display_controller.js
+++ ionic/qurantools/www/app/components/inferences/inference_display_controller.js
@@ -1,5 +1,5 @@
 angular.module('ionicApp')
-    .controller('InferenceDisplayController', function ($scope, $routeParams, $location, authorization, localStorageService,  Restangular, $timeout,$sce,$ionicModal) {
+    .controller('InferenceDisplayController', function ($scope, $routeParams, $location, authorization, localStorageService,  Restangular, $timeout,$sce,$ionicModal,$ionicPopup) {
 
         //All scope variables
         $scope.inferenceId=0;
@@ -38,10 +38,16 @@ angular.module('ionicApp')
 
             var inferenceRestangular = Restangular.one("inferences", $scope.inferenceId);
             inferenceRestangular.customDELETE("", {}, {'access_token': $scope.access_token}).then(function (data) {
-                $location.path('inferences/');
+                if(!config_data.isMobile){
+                    $location.path('inferences/');
+                }else {
+                    $location.path('m_inference/');
+                }
             });
 
         }
+
+
 
         //tags input auto complete
         $scope.peoplelist = function (people_name) {
@@ -59,7 +65,13 @@ angular.module('ionicApp')
 
         //Edit inference
         $scope.edit_inference = function () {
-            $location.path('inference/edit/'+$scope.inferenceId+"/");
+            if(!config_data.isMobile){
+                $location.path('inference/edit/'+$scope.inferenceId+"/");
+            }else
+            {
+                $location.path('m_inference/edit/'+$scope.inferenceId+"/");
+            }
+
         }
 
         $scope.compileContent = function(original,verseList, verseIdList, inline){
@@ -342,9 +354,38 @@ angular.module('ionicApp')
                 author: $scope.referenced.selectedAuthor
 
             }
-            $location.path("/inference/display/"+$scope.inferenceId+"/", false).search(parameters);
+            if(!config_data.isMobile){
+                $location.path("/inference/display/"+$scope.inferenceId+"/", false).search(parameters);
+            }else{
+                $location.path("/m_inference/display/"+$scope.inferenceId+"/", false).search(parameters);
+            }
         };
 
+        if(config_data.isMobile){
+
+            $scope.deleted = function() {
+                var confirmPopup = $ionicPopup.confirm({
+                    title: 'Notu Silme',
+                    template: 'Çıkarım notu silinecek Onaylıyor musunuz?',
+                    buttons: [{
+                        text : 'Vazgec',
+                        type: 'button-balanced',
+                    },
+                        {
+                            text : 'Sil',
+                            type : 'button-assertive'
+                        }]
+                });
+
+                confirmPopup.then(function(res) {
+                    if(res) {
+                        $scope.delete_inference();
+                    } else {
+                        console.log('You are not sure');
+                    }
+                });
+            };
+        }
         $scope.getChapterVerseNotation = function(verseId){
             return Math.floor(verseId/1000)+":"+ verseId%1000;
         };
